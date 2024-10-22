dccde847e379bca5639c7c49d8f619745bfcc9f8
Caua Mariz DEV <marizcaua88@gmail.com>
2024-10-22 19:29:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
-function SeeProjectsContt(){
-  location.href='projts.html'
+function SeeProjectsContt() {
+  location.href = 'projts.html'
 }
+
+let AreaTexto = document.querySelector("#Texto");
+let AreaExibeTexto = document.querySelector("#AreaExibeTexto");
+
+function ContagemCaracteres() {
+  AreaExibeTexto.textContent = AreaTexto.value.length + '/200';
+}
+
+ContagemCaracteres()
+
+AreaTexto.addEventListener('input', ContagemCaracteres)
+
+AreaTexto.addEventListener('input', function () {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+  this.style.scrollHeight = '0'
+})
