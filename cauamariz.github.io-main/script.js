2f70e47b95e3cd96a180e24af777f6fecf7ebcb4
caua <marizcaua0@gmail.com>
2025-06-05 19:04:09 -0300

    Alteracao dos arquivos

---
+++ cauamariz.github.io-main/script.js
@@ -0,0 +1,24 @@
+
+function SeeProjectsContt() {
+  location.href = 'projts.html'
+}
+
+let AreaTexto = document.querySelector("#Texto");
+let AreaExibeTexto = document.querySelector("#AreaExibeTexto");
+
+function ContagemCaracteres() {
+  AreaExibeTexto.textContent = AreaTexto.value.length + '/200';
+}
+
+ContagemCaracteres()
+
+AreaTexto.addEventListener('input', ContagemCaracteres)
+
+AreaTexto.addEventListener('input', function () {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+  this.style.scrollHeight = '0'
+})
+AreaTexto.className = "AreaTexto";
+
+
